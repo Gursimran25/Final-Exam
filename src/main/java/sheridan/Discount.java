edccdbae6d195b94042aa7c78385c9034f8ab53d
Gursimran25 <gursimran9007@gmail.com>
2020-08-14 16:07:00 -0400

    Created a new class to display the discount amount: G.K.

---
+++ src/main/java/sheridan/Discount.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package sheridan;
+
+/**
+ *
+ * @author User
+ */
+   public class Discount
+{
+	public static void main(String args[])
+	{
+ 
+	double  dis,amount,markedprice,s;
+               
+	markedprice=1000;
+ 
+        dis=25;  // 25 mean 25%			
+	
+	System.out.println("markedprice= "+markedprice);
+ 
+	System.out.println("discount rate="+dis);
+         
+        s=100-dis;
+ 
+	amount= (s*markedprice)/100;
+ 
+	System.out.println("amount after discount="+amount);
+ 
+	}
+}
+
+}
