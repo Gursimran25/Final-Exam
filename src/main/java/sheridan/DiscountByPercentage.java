2eb6a142167dcc37cf9b8e541693df1daae63829
Gursimran25 <gursimran9007@gmail.com>
2020-08-14 16:07:29 -0400

    Created a new class to display the discount in percentage: G.K.

---
+++ src/main/java/sheridan/DiscountByPercentage.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package sheridan;
+
+/**
+ *
+ * @author User
+ */
+java.util.Scanner;
+ 
+public class DiscountByPercentage
+{
+	public static void main(String args[])
+	{
+ 
+	double  dis,amount,markedprice,s;
+	
+	Scanner sc=new Scanner(System.in);
+	
+	System.out.println("enter markedprice ");	
+               
+	markedprice=sc.nextDouble();
+ 
+        System.out.println("enter discount percentage ");	
+               
+	dis=sc.nextDouble();			
+	
+        s=100-dis;
+ 
+	amount= (s*markedprice)/100;
+ 
+	System.out.println("amount after discount="+amount);
+ 
+	}
+}
+
+
